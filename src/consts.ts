cbacacbe33d93c9a18fac2ec82b86b3b8f508a3a
sandrfunder <hello@fundwaltz.com>
2026-05-31 15:56:45 +0300

    Update site configuration: changed email, social links, and metadata titles/descriptions for Alex Kshutashvili

--- src/consts.ts
+++ src/consts.ts
@@ -2,43 +2,43 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Alex Kshutashvili",
-  EMAIL: "markhorn.dev@gmail.com",
+  EMAIL: "alex.kshutashvili@inbox.eu",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "I am a minimal, SEO-friendly, accessible portfolio and blog for developers. I am an even more lightweight version of my popular theme Astro Sphere. I have Lighthouse scores of 100 across the board for performance, accessibility, best practices and SEO.",
+  TITLE: "Home | Alex Kshutasvili",
+  DESCRIPTION: "The home page of Alex Kshutashvili: entrepreneur, minimalist, human.",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "10+ years fintech experience. Founded Finceh LLC (2018) and co-founded Neofin (2022), a no-code lending automation platform. Built solutions for banks like Unexbank and MTB Bank. Expert in launching 22+ fintech projects.",
+  DESCRIPTION: "The blog of Alex Kshutashvili: Explore a personal collection of writings, insightful musings, and ideas.",
 };
 
 export const WORK: Metadata = {
   TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+  DESCRIPTION: "The timeline of Alex Kshutashvili: Ventures, triumphs, challenges, and milestones that shaped my life.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION: "The Projects of Alex Kshutashvili: Driving innovation across Fintech, Neobanks, Lending Automation, and Mobile Platforms.",
 };
 
 export const SOCIALS: Socials = [
   { 
     NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://x.com/kshutashv1l1",
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/sandrfunder/awesome-company-as-a-service"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/kshutashvili",
   }
 ];
