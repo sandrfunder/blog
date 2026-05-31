278d4d21eb4bdc5113af755e54e481afda05d222
sandrfunder <hello@fundwaltz.com>
2026-05-31 20:15:58 +0300

    Update copyright year from 2024 to 2026 in footer component

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Workoholic from Luhansk",
+  NAME: "kshutashvili.com",
   EMAIL: "alex.kshutashvili@inbox.eu",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
--- src/content/blog/01-getting-started/index.md
+++ src/content/blog/01-getting-started/index.md
@@ -1,7 +1,7 @@
 ---
 title: "Getting started"
 description: "Hit the ground running."
-date: "Mar 22 2024"
+date: "May 31 2026"
 ---
 
 The basic configuration of Nano is pretty simple.
